fa40e1726f94c4b852bd2eb2de1f20db90e5a729
masako.o <msxlucky@gmail.com>
2024-05-13 00:29:16 +0900

    apply git ignore

---
+++ detail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <header>
+      <title>Detail</title>
+    </header>
+    <body>
+      <h1>詳細ページ</h1>
+      <br>
+      <a href="index.html">もどるヨ〜</a>
+    </body>
+</html>
