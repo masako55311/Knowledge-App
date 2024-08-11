20890645df77bde9b16fc409856739d1ffb5b047
masako.o <msxlucky@gmail.com>
2024-08-11 23:29:15 +0900

    20240811 newpost.php modified

--- newpost.php
+++ newpost.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <header>
-      <title>Detail</title>
-    </header>
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/base.css">
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/base.js"></script>
+      <title>knowledge hub</title>
+    </head>
     <body>
-      <h1>新規作成ページ</h1>
-      <br>
-      <a href="index.html">もどるヨ〜</a>
+      <div class="container-md">
+        <!-- As a link -->
+      <div class="row">
+        <nav class="navbar bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Knowledge Hub</a>
+          </div>
+        </nav>
+      </div>
+      <!-- main-->
+      <div class="row">
+             <h1>新規作成画面です</h1>
+      </div> 
+    </div>
+      <a href="./index.php">back</a>
     </body>
 </html>
